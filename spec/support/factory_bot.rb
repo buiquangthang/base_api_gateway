3b06a8178b34f8725bb4a6aeaaa893717d6081fc
THANG BUI QUANG <thang.bui@oivan.com>
2023-02-20 15:33:41 +0700

    Add testing gem and configuration

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,8 @@
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+  include ActiveSupport::Testing::FileFixtures
+end
